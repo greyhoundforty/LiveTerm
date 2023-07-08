72e0602ef0752c9c9c99264395cf745c855e6f5d
greyhoundforty <ryantiffany@fastmail.com>
2023-07-08 18:58:01 -0500

    chore(config.json): update title to "ryan tiffany dot dev" and add "threads" to social links chore(config.json): update email to "admin@ryantiffany.dev" and ps1_hostname to "the-good-place" chore(sumfetch.ts): update ASCII art and welcome message The config.json file has been updated to reflect the changes in the title, email, and ps1_hostname fields. The social links have also been updated to include a new "threads" link. This improves the accuracy and consistency of the configuration.
    In the sumfetch.ts file, the ASCII art and welcome message have been updated to reflect a new welcome message and ASCII art. This enhances the user experience and provides a more personalized touch to the application.

--- src/utils/bin/sumfetch.ts
+++ src/utils/bin/sumfetch.ts
@@ -6,14 +6,14 @@ const sumfetch = async (args: string[]): Promise<string> => {
         ▄▓▓▀ ▄▓▓▀▓▓▓▀▓▓▄ ▀▀▓▓▄              -----------
       ▓▓▀  ▄▓▀   ▐▓▓  ▀▓▓    ▓▓▄             ABOUT
     ▄▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            ${config.name}
-   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓           <u><a href="${config.resume_url}" target="_blank">resume</a></u>
+   ▓▓     ▓▓▓    ▐▓▓    ▐▓▓     ▓▓           ${config.title}
 ▐▓▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▓       爵 <u><a href="${config.repo}" target="_blank">Github repo</a></u>
 ▐▓                                 ▐▓       -----------
-▐▓        > L I V E T E R M        ▐▓        CONTACT 
+▐▓        > W E L C O M E          ▐▓        CONTACT 
 ▐▓                                 ▐▓        <u><a href="mailto:${config.email}" target="_blank">${config.email}</a></u>
 ▐▓▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▓        <u><a href="https://github.com/${config.social.github}" target="_blank">github.com/${config.social.github}</a></u>
    ▓▓      ▐▓▓    ▓▓    ▐▓▓     ▓▓           <u><a href="https://linkedin.com/in/${config.social.linkedin}" target="_blank">linkedin.com/in/${config.social.linkedin}</a></u>
-    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+    ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓            <u><a href="https://www.threads.net/@${config.social.threads}" target="_blank">www.threads.net/@greyhound40${config.social.threads}</a></u>
       ▓▓▓   ▐▓▓   ▓▓   ▓▓▓   ▓▓▀            -----------
         ▀▓▓▄▄ ▀▓▓▄▓▓▄▓▓▓▄▄▓▓▀               
             ▀▓▓▓▓▓▓▓▓▓▓▓▀▀                  
